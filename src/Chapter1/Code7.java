a910e0ea0a385f978a82cbf9d8c32c250a42a5e2
moonjaeseong <wotmdskfk32@naver.com>
2023-03-19 19:15:01 +0900

    code7

--- src/Chapter1/Code7.java
+++ src/Chapter1/Code7.java
@@ -1,4 +1,20 @@
 package Chapter1;
 
+import java.util.Scanner;
+
 public class Code7 {
-}
+
+    public static void main(String[] args){
+
+    Scanner sc = new Scanner(System.in);
+        System.out.print("숫자 입력 : ");
+        int n = sc.nextInt();
+        int sum=0;
+
+        sum = (n+1) * (n/2) +(n%2==1?n/2 +1:0);
+
+        System.out.println(sum);
+
+        }
+    }
+
